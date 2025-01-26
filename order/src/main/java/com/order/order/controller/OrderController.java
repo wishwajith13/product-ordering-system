db4a69b2afda6f37e59f8e19d732ddcbdebb49ca
wishwajith13 <151111080+wishwajith13@users.noreply.github.com>
2025-01-26 17:03:53 +0530

    first commit

---
+++ order/src/main/java/com/order/order/controller/OrderController.java
@@ -0,0 +1,37 @@
+package com.order.order.controller;
+
+
+import com.order.order.dto.OrderDTO;
+import com.order.order.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "api/v1/")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/getorders")
+    public List<OrderDTO> getOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping("/order/{orderId}")
+    public OrderDTO getOrderById(@PathVariable Integer orderId) {
+        return orderService.getOrderById(orderId);
+    }
+
+    @PutMapping("/updateorder")
+    public OrderDTO updateOrder(@RequestBody OrderDTO orderDTO) {
+        return orderService.updateOrder(orderDTO);
+    }
+
+    @DeleteMapping("/deleteorder/{orderId}")
+    public String deleteOrder(@PathVariable Integer orderId) {
+        return orderService.deleteOrder(orderId);
+    }
+}
---
+++ order/src/main/java/com/order/order/service/OrderService.java
@@ -0,0 +1,55 @@
+package com.order.order.service;
+
+
+import com.order.order.dto.OrderDTO;
+import com.order.order.model.Orders;
+import com.order.order.repo.OrderRepo;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+import java.util.List;
+
+@Service
+@Transactional
+public class OrderService {
+    private final WebClient inventoryWebClient;
+    private final WebClient productWebClient;
+
+    @Autowired
+    private OrderRepo orderRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public OrderService(WebClient inventoryWebClient, WebClient productWebClient, OrderRepo orderRepo, ModelMapper modelMapper) {
+        this.inventoryWebClient = inventoryWebClient;
+        this.productWebClient = productWebClient;
+        this.orderRepo = orderRepo;
+        this.modelMapper = modelMapper;
+    }
+
+    public List<OrderDTO> getAllOrders() {
+        List<Orders>orderList = orderRepo.findAll();
+        return modelMapper.map(orderList, new TypeToken<List<OrderDTO>>(){}.getType());
+    }
+
+    public OrderDTO updateOrder(OrderDTO OrderDTO) {
+        orderRepo.save(modelMapper.map(OrderDTO, Orders.class));
+        return OrderDTO;
+    }
+
+    public String deleteOrder(Integer orderId) {
+        orderRepo.deleteById(orderId);
+        return "Order deleted";
+    }
+
+    public OrderDTO getOrderById(Integer orderId) {
+        Orders order = orderRepo.getOrderById(orderId);
+        return modelMapper.map(order, OrderDTO.class);
+    }
+}
